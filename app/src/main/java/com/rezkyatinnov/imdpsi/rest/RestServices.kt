8e7d34a9a231596c63981863af59fb38b84237d3
Mohamad Rezkya Feurstinnov Hairy <rezkyatinnov@gmail.com>
2017-10-31 02:35:19 +0800

    setting up new project

---
+++ app/src/main/java/com/rezkyatinnov/imdpsi/rest/RestServices.kt
@@ -0,0 +1,10 @@
+package com.rezkyatinnov.imdpsi.rest
+
+/**
+ * Created by rezkyatinnov on 30/10/2017.
+ *
+ * All endpoint services defined here using retrofit interface style
+ */
+interface RestServices {
+
+}
